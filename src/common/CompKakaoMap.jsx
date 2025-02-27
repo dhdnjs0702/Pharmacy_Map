f432bd3defc83defbe0dda1c25ea5e033975baa7
mzsoba <owt4832@gmail.com>
2025-02-27 14:14:08 +0900

    feat/ 카카오맵 예제 추가

--- src/common/CompKakaoMap.jsx
+++ src/common/CompKakaoMap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 const { kakao } = window;
 const CompKakaoMap = () => {
   const container = useRef(null);
