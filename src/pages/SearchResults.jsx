6d8e56a76f27cc99a169bb1ebfb6ea7461138403
hj <hyeonjubaek06@gmail.com>
2025-02-28 03:59:34 +0900

    feat: SearchResults 컴포넌트의 스타일 수정 및 KakaoMap 배경 설정

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -6,12 +6,13 @@ import "../index.css";
 
 const SearchResults = () => {
   return (
-    <div>
+    <div className="overflow-hidden min-h-screen">
       <CompNavBar />
       <div className="relative">
         {/* KakaoMap가 전체 배경을 채우고, 그 위에 좌측 패널이 모달처럼 떠 있도록 */}
         <KakaoMap />
-        <div className="absolute top-5 left-5 w-[28.57%] p-4 bg-white shadow-md z-10">
+        <div className="absolute top-5 left-5 w-[28.57%] h-[90vh] 
+                        p-4 bg-white shadow-md z-10 rounded-lg">
           <CompSearchBar />
         </div>
       </div>
