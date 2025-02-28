79afacc3553305c0d5369df0f9b415c9f40f249e
JDY <wjsehdud@nate.com>
2025-02-28 14:38:34 +0900

    feat: 회원가입 페이지에 폼 컴포넌트 추가 및 상태 관리 구현

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    id: "",
+    password: "",
+    nickname: "",
+  });
 
-export default SignUpPage
+  const ChangeHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <h1>회원가입을 해주세요!</h1>
+
+      <form className="mt-8 flex flex-col gap-3">
+        <input
+          type="text"
+          name="id"
+          value={formData.id}
+          onChange={ChangeHandler}
+          placeholder="로그인 시 사용되는 아이디입니다."
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={ChangeHandler}
+          placeholder="로그인 시 사용되는 비밀번호입니다."
+        />
+        <input
+          type="text"
+          name="nickname"
+          value={formData.nickname}
+          onChange={ChangeHandler}
+          placeholder="로그인 시 사용되는 닉네임입니다."
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SignUpPage;
