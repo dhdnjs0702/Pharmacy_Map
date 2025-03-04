6bdef3e89d451604f7e6343f6fe830b809a85858
mzsoba <owt4832@gmail.com>
2025-03-04 10:50:51 +0900

    feat/ 컴포넌트 분리

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,28 +1,48 @@
-import { Map } from "react-kakao-maps-sdk";
-import AddMapClickEvent from "../customhook/searchresults/searchresultshooks";
+import { useEffect } from "react";
+import "../KakaoMap.css";
+import Swal from "sweetalert2";
+import CompMap from "../components/searchresults/CompMap";
+import CompSearchBar from "../common/CompSearchBar";
+import CompList from "../components/searchresults/CompList";
+import CompPagination from "../components/searchresults/CompPagination";
+import useKakaoSearch from "../customhook/searchresults/useKakaoSearch";
+import CompNavBar from "../common/CompNavBar";
+import { useKakaoStore } from "../zustand/dragon";
 
 const SearchResults = () => {
-  const { result, mapClickHandler } = AddMapClickEvent();
+  const { error, searchPharmacies, handlePaginationClick } = useKakaoSearch();
+  const { isLoading } = useKakaoStore();
+  useEffect(() => {
+    if (error) {
+      Swal.fire({
+        title: "에러",
+        text: "에러가 발생했습니다. 관리자에게 문의 해주세요",
+        icon: "error",
+        confirmButtonText: "확인",
+        confirmButtonColor: "#3085d6",
+      });
+    }
+  }, [error]);
+
   return (
     <>
-      <Map // 지도를 표시할 Container
-        center={{
-          // 지도의 중심좌표
-          lat: 37.521989659684756,
-          lng: 126.97453040112002, //이촌역 기준
-        }}
-        style={{
-          // 지도의 크기
-          width: "100%",
-          height: "450px",
-        }}
-        level={2} // 지도의 확대 레벨
-        onClick={mapClickHandler}
-      />
-      <p>
-        <em>지도를 클릭해주세요!</em>
-      </p>
-      <p id="result">{result}</p>
+      <CompNavBar />
+      <div className="map_wrap">
+        <CompMap />
+
+        <div id="menu_wrap" className="bg_white">
+          <CompSearchBar onSearch={searchPharmacies} />
+
+          {isLoading ? (
+            <div className="loading">검색 중...</div>
+          ) : (
+            <>
+              <CompList /> {/*디테일 페이지는 complist에서 쿼리 스트링 참조*/}
+              <CompPagination onPageChange={handlePaginationClick} />
+            </>
+          )}
+        </div>
+      </div>
     </>
   );
 };
